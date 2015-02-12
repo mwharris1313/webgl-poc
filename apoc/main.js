b46e2d63b694a587a2530e10756d3a6afd5d1ab1
Michael Harris <mwharris333@gmail.com>
2015-02-12 00:39:53 -0500

    added webgl context creation with check for context creation errors
    Context creation error event, spec available at:
    https://www.khronos.org/registry/webgl/specs/1.0/

--- apoc/main.js
+++ apoc/main.js
@@ -1,3 +1,26 @@
+// WebGL spec available at https://www.khronos.org/registry/webgl/specs/1.0/
 function main(){
-	console.log("test");
+
+	log("START main()...");
+
+	var canvas = document.getElementById('glCanvas');
+
+	var errorInfo = "";
+	function onContextCreationError(event) {
+		canvas.removeEventListener("webglcontextcreationerror", onContextCreationError, false);
+		errorInfo = e.statusMessage || "Unknown";
+	}
+	canvas.addEventListener("webglcontextcreationerror", onContextCreationError, false);
+
+	var gl = canvas.getContext("experimental-webgl");
+	if(!gl) {
+		alert("A WebGL context could not be created.\nReason: " + errorInfo);
+	}
+
+	canvas.width = 640;
+	canvas.height = 360;
+	gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+
 }
+
+var log = console.log.bind(console);

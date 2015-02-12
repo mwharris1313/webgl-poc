95a830eff0d73ef1aa6ac72fa239004ed15145de
Michael Harris <mwharris333@gmail.com>
2015-02-12 15:48:28 -0500

    Absorbed createShader into createShaderEmbedded

--- bpoc/glUtil.js
+++ bpoc/glUtil.js
@@ -1,19 +1,35 @@
 "use strict";
 
-var createShader = function(gl, src, type) {
+var createShaderEmbedded = function(gl, id) {
 
-	var shader = gl.createShader(type);
-	gl.shaderSource(shader, src);
-	gl.compileShader(shader);
+	var tag = document.getElementById(id);
+	if (!tag) throw("ERROR: Unknown Tag ID : " + id);
 
-	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-		console.log("ERROR: Compiling Shader [" + shader + "] : " + gl.getShaderInfoLog(shader));
-		gl.deleteShader(shader);
+	var shaderType = {"Vertex-Shader":gl.VERTEX_SHADER, "Fragment-Shader":gl.FRAGMENT_SHADER}
+	var type = shaderType[tag.type];
+	if (type === undefined) {
+		throw("ERROR: Unknown Shader Type");
 		return null;
 	}
 
-	return shader;
-}
+	var src = tag.text;
+	var getShader = function(src, type){
+		var shader = gl.createShader(type);
+		gl.shaderSource(shader, src);
+		gl.compileShader(shader);
+
+		if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+			console.log("ERROR: Compiling Shader [" + shader + "] : " + gl.getShaderInfoLog(shader));
+			gl.deleteShader(shader);
+			return null;
+		}
+
+		return shader;
+	}
+
+	return getShader(src, type);
+
+};
 
 var createProgram = function(gl, shaders, attribs, locations) {
 
@@ -37,20 +53,4 @@ var createProgram = function(gl, shaders, attribs, locations) {
 	}
 
 	return program;
-};
-
-var createShaderEmbedded = function(gl, id) {
-
-	var tag = document.getElementById(id);
-	if (!tag) throw("ERROR: Unknown Tag ID : " + id);
-
-	var shaderType = {"Vertex-Shader":gl.VERTEX_SHADER, "Fragment-Shader":gl.FRAGMENT_SHADER}
-	var type = shaderType[tag.type];
-	if (type === undefined) {
-		throw("ERROR: Unknown Shader Type");
-		return null;
-	}
-
-	var src = tag.text;
-	return createShader(gl, src, type);
 };

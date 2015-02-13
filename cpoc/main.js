919e3d8d1100fab19a3f72a9e47b2dd1a044be61
Michael Harris <mwharris333@gmail.com>
2015-02-12 20:11:04 -0500

    Experimenting with image rendering
    Experimenting with a Web Tutorial Image Render Example from
    http://webglfundamentals.org/webgl/lessons/webgl-image-processing.html

--- cpoc/main.js
+++ cpoc/main.js
@@ -1,7 +1,16 @@
-// WebGL spec available at https://www.khronos.org/registry/webgl/specs/1.0/
-function main(){
+"use strict";
 
-	log("START main()...");
+var log = console.log.bind(console);
+
+window.onload = function(){
+	var image = new Image();
+	image.src = "kitty.jpg";
+	image.onload = function() {
+	render(image);
+	}
+}
+
+function render(image) {
 
 	var canvas = document.getElementById('glCanvas');
 
@@ -13,46 +22,88 @@ function main(){
 	canvas.addEventListener("webglcontextcreationerror", onContextCreationError, false);
 
 	var gl = canvas.getContext("experimental-webgl");
-	if(!gl) alert("A WebGL context could not be created.\nReason: " + errorInfo);
+	if(!gl) alert("ERROR: WebGL Context Not Created : " + errorInfo);
 
 	canvas.width = 640;
 	canvas.height = 360;
 	gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-
-	// =======================================================================
-	// setup a GLSL program
 
 	var vertexShader = createShaderEmbedded(gl, "VertexShader");
 	var fragmentShader = createShaderEmbedded(gl, "FragmentShader");
 	var program = createProgram(gl, [vertexShader, fragmentShader]);
 	gl.useProgram(program);
 
+	// -------------------------------------------------------------------- From Tutorial Example for further study
 	// look up where the vertex data needs to go.
-	var positionLocation = gl.getAttribLocation(program, "position");
+	var positionLocation = gl.getAttribLocation(program, "a_position");
+	var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
 
-	// Create a buffer and put a single clipspace rectangle in it (2 triangles)
-	var buffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	// provide texture coordinates for the rectangle.
+	var texCoordBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
 	gl.bufferData(
-		gl.ARRAY_BUFFER, 
+		gl.ARRAY_BUFFER,
 		new Float32Array([
-			-1.0, -1.0, 
-			 1.0, -1.0, 
-			-1.0,  1.0, 
-			-1.0,  1.0, 
-			 1.0, -1.0, 
-			 1.0,  1.0
-		]),
+			0.0, 0.0,
+			1.0, 0.0,
+			0.0, 1.0,
+			0.0, 1.0,
+			1.0, 0.0,
+			1.0, 1.0]),
 		gl.STATIC_DRAW
 	);
+
+	gl.enableVertexAttribArray(texCoordLocation);
+	gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+	// Create a texture.
+	var texture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+
+	// Set the parameters so we can render any size image.
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+	// Upload the image into the texture.
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+	// lookup uniforms
+	var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+
+	// set the resolution
+	gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
+
+	// Create a buffer for the position of the rectangle corners.
+	var buffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 	gl.enableVertexAttribArray(positionLocation);
 	gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
-	gl.drawArrays(gl.TRIANGLES, 0, 6);
+	// Set a rectangle the same size as the image.
+	var setRectangle = function(gl, x, y, width, height){
+		var x1 = x;
+		var x2 = x + width;
+		var y1 = y;
+		var y2 = y + height;
+		gl.bufferData(
+			gl.ARRAY_BUFFER,
+			new Float32Array([
+				x1, y1,
+				x2, y1,
+				x1, y2,
+				x1, y2,
+				x2, y1,
+				x2, y2
+			]),
+			gl.STATIC_DRAW
+		);
+	}
+	setRectangle(gl, 0, 0, image.width, image.height);
 
-	// =======================================================================
-	log("END main()...");
+	// Draw the rectangle.
+	gl.drawArrays(gl.TRIANGLES, 0, 6);
+	// --------------------------------------------------------------------
 
 }
-
-var log = console.log.bind(console);

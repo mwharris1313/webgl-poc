030904e2fad310e2ec7178d5e6320ace9ceda5ed
Michael Harris <mwharris333@gmail.com>
2015-03-08 04:29:13 -0400

    trying to pull as much out of the render loop to isolate cause of slowdown , has to do with the tile atlas being (2048px, 2048px) vs (640px,360px) possibly related to loading texture to vram? cpu to gpu transfers?

--- poc/main.js
+++ poc/main.js
@@ -2,10 +2,26 @@
 var log = console.log.bind(console);
 var g, gl, program;
 var canvas;
+var texArray = [];
+var texCoordBuffer;
+var array = [];
+var texture;
+var buffer;
+
+// look up where the vertex data needs to go.
+var positionLocation;
+var texCoordLocation;
+var screenLocation;
+var mouseLocation;
+
+
+
+
+
 
 g = {
-    isDebug: true,
-    isProfiling: false,
+    isDebug: false,
+    isProfiling: true,
     screen: {width:640, height:360},
     tile: {width:32, height:32, rows:13, cols:21},
     frame: {count:0, tLast:0, profileCount:15, repeat:1},
@@ -63,21 +79,66 @@ function canvasXY(tag) {
 function onClick(e){
     g.mouse.x = e.clientX-g.canvas.offsetLeft;
     g.mouse.y = e.clientY-g.canvas.offsetTop;
-    log('onClick', g.mouse);
+    if (g.isDebug) log('onClick', g.mouse);
     g.toggle.onClick = !g.toggle.onClick;
 }
 function onMouseMove(e){
     g.mouse.x = e.clientX-g.canvas.offsetLeft;
     g.mouse.y = e.clientY-g.canvas.offsetTop;
-    log('onMouseMove', g.mouse);
+    if (g.isDebug) log('onMouseMove', g.mouse);
 }
 function onTouchStart(e){
-    log('onTouchStart',canvasXY(e.clientX,e.clientY));
+    if (g.isDebug) log('onTouchStart',canvasXY(e.clientX,e.clientY));
 }
 function onTouchMove(e){
-    log('onTouchMove',canvasXY(e.clientX,e.clientY));
+    if (g.isDebug) log('onTouchMove',canvasXY(e.clientX,e.clientY));
 }
 
+
+// Set an image's position and dimensions. 
+var setTileTexture = function(col,row,arr){
+    var x1,y1,x2,y2,x,y,tw,th;
+    tw = g.screen.width / g.atlas.width;
+    th = g.screen.height / g.atlas.height;
+    x = 0;
+    y = 0;
+    x1 = x;
+    x2 = x + tw;
+    y1 = y;
+    y2 = y + th;
+
+    arr.push(
+            x1, y1,
+            x2, y1,
+            x1, y2,
+            x1, y2,
+            x2, y1,
+            x2, y2
+    );
+
+}
+// Set an image's position and dimensions. 
+var setTile = function(row,col,arr){
+    var x1,y1,x2,y2,x,y,tw,th;
+    tw = g.screen.width;
+    th = g.screen.height;
+    x = 0;
+    y = 0;
+    x1 = x;
+    x2 = x + tw;
+    y1 = y;
+    y2 = y + th;
+
+    arr.push(
+            x1, y1,
+            x2, y1,
+            x1, y2,
+            x1, y2,
+            x2, y1,
+            x2, y2
+    );
+
+}
 
 // =================================================================
 function init(){
@@ -105,8 +166,6 @@ function init(){
     program = createProgram(gl, [vertexShader, fragmentShader]);
     gl.useProgram(program);
 
-
-
     var xy = canvasXY(canvas);
     g.canvas.offsetLeft = xy[0];
     g.canvas.offsetTop = xy[1];
@@ -119,104 +178,24 @@ function init(){
     canvas.addEventListener('touchstart', onTouchStart, false);
     canvas.addEventListener('touchmove', onTouchMove, false);
 
-}
-
-
-// =================================================================
-function render() {
-    requestAnimationFrame(render);
-
-    msPerFrame();
-
-for (var repeat=0; repeat<g.frame.repeat; repeat++){
-
+    // provide texture coordinates for the rectangle.
+    texCoordBuffer = gl.createBuffer();
 
     // look up where the vertex data needs to go.
-    var positionLocation = gl.getAttribLocation(program, "inPosition");
-    var texCoordLocation = gl.getAttribLocation(program, "inTexCoord");
+    positionLocation = gl.getAttribLocation(program, "inPosition");
+    texCoordLocation = gl.getAttribLocation(program, "inTexCoord");
 
     // uniforms
-    var screenLocation = gl.getUniformLocation(program, "uScreen");
+    screenLocation = gl.getUniformLocation(program, "uScreen");
     gl.uniform2f(screenLocation, g.screen.width, g.screen.height);
 
-    var mouseLocation = gl.getUniformLocation(program, "uMouse");
+    mouseLocation = gl.getUniformLocation(program, "uMouse");
     gl.uniform2f(mouseLocation, g.mouse.x, g.mouse.y);
 
 
-
-    // Set an image's position and dimensions. 
-    var setTileTexture = function(col,row,arr){
-        var x1,y1,x2,y2,x,y,tw,th;
-        tw = g.tile.width / g.atlas.width;
-        th = g.tile.height / g.atlas.height;
-        x = (col-1)*tw;
-        y = (row-1)*th;
-        x1 = x;
-        x2 = x + tw;
-        y1 = y;
-        y2 = y + th;
-
-        arr.push(
-                x1, y1,
-                x2, y1,
-                x1, y2,
-                x1, y2,
-                x2, y1,
-                x2, y2
-        );
-
-    }
-
-    // Set an image's position and dimensions. 
-    var setTileTextureNew = function(col,row,arr){
-        var x1,y1,x2,y2,x,y,tw,th;
-        tw = g.screen.width / g.atlas.width;
-        th = g.screen.height / g.atlas.height;
-        x = 0;
-        y = 0;
-        x1 = x;
-        x2 = x + tw;
-        y1 = y;
-        y2 = y + th;
-
-        arr.push(
-                x1, y1,
-                x2, y1,
-                x1, y2,
-                x1, y2,
-                x2, y1,
-                x2, y2
-        );
-
-    }
-    // Set an image's position and dimensions. 
-    var setTileNew = function(row,col,arr){
-        var x1,y1,x2,y2,x,y,tw,th;
-        tw = g.screen.width;
-        th = g.screen.height;
-        x = 0;
-        y = 0;
-        x1 = x;
-        x2 = x + tw;
-        y1 = y;
-        y2 = y + th;
-
-        arr.push(
-                x1, y1,
-                x2, y1,
-                x1, y2,
-                x1, y2,
-                x2, y1,
-                x2, y2
-        );
-
-    }
-
-    var texArray = [];
-    setTileTextureNew(1,1,texArray);
+    setTileTexture(1,1,texArray);
 
     // provide texture coordinates for the rectangle.
-    var texCoordBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
     gl.bufferData(
         gl.ARRAY_BUFFER,
@@ -227,7 +206,7 @@ for (var repeat=0; repeat<g.frame.repeat; repeat++){
     gl.enableVertexAttribArray(texCoordLocation);
     gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
 
-    var texture = gl.createTexture();
+    texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
 
     // Set the parameters so we can render any size image.
@@ -236,42 +215,12 @@ for (var repeat=0; repeat<g.frame.repeat; repeat++){
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image); // image into the texture.
-
 
     // Create a buffer for the position of the rectangle corners.
-    var buffer = gl.createBuffer();
+    buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.enableVertexAttribArray(positionLocation);
-    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
-    // Set an image's position and dimensions. 
-    var setTile = function(row,col,arr){
-        var x1,y1,x2,y2,x,y,tw,th;
-        tw = g.tile.width;
-        th = g.tile.height;
-        x = (col-1)*tw;
-        y = (row-1)*th;
-        x1 = x;
-        x2 = x + tw;
-        y1 = y;
-        y2 = y + th;
-
-        arr.push(
-                x1, y1,
-                x2, y1,
-                x1, y2,
-                x1, y2,
-                x2, y1,
-                x2, y2
-        );
-
-    }
-
-    var array = [];
-
-    setTileNew(1,1,array);
-
+    setTile(1,1,array);
 
     gl.bufferData(
         gl.ARRAY_BUFFER,
@@ -279,11 +228,39 @@ for (var repeat=0; repeat<g.frame.repeat; repeat++){
         gl.STATIC_DRAW
     );
 
-    var numVerts = array.length / 2;
-    gl.drawArrays(gl.TRIANGLES, 0, numVerts);
 
-    // -----------------------------------------------------------------
 }
+// =================================================================
+function render() {
+    requestAnimationFrame(render);
+
+    msPerFrame();
+
+    for (var repeat=0; repeat<g.frame.repeat; repeat++){
+
+        gl.uniform2f(mouseLocation, g.mouse.x, g.mouse.y);
+
+        // provide texture coordinates for the rectangle.
+        gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+
+        gl.enableVertexAttribArray(texCoordLocation);
+        gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image); // image into the texture.
+
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.enableVertexAttribArray(positionLocation);
+        gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+        var numVerts = array.length / 2;
+        gl.drawArrays(gl.TRIANGLES, 0, numVerts);
+
+
+        // -----------------------------------------------------------------
+    }
 
 }
 

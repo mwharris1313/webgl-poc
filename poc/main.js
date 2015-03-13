077fedbb55e460c98f357a3028ff61803a890816
Michael Harris <mwharris333@gmail.com>
2015-03-13 01:41:08 -0400

    experimenting with multiple textures ,
    want to eventually load data into fragment shader via texture

--- poc/main.js
+++ poc/main.js
@@ -15,6 +15,11 @@ var buffer;
 var frameBuffer;
 var image;
 var atlas;
+var tileMap;
+var tileMapTexture;
+var tileMapBuffer;
+var tileMapArray = [];
+var tileMapImage;
 
 // look up where the vertex data needs to go.
 var positionLocation;
@@ -22,25 +27,23 @@ var screenVertexLocation;
 var screenLocation;
 var mouseLocation;
 var cameraLocation;
+var tileLocation;
+
+var atlasTextureLocation;
+var tileMapTextureLocation;
+
+
 
 var posX = 0;
 var posY = 0;
+
 
 // ***********************************************************
 window.onload = function(){
     var thisFunc = 'windows.onload()';
     dbg.func(thisFile, thisFunc);
 
-    atlas = new Image();
-    atlas.src = "../assets/atlas2048_32.png";
-    atlas.onload = function() {
-
-    // image = new Image();
-    // image.src = "../assets/atlas2048_32.png";
-    // image.onload = function() {
-    //     window.requestAnimationFrame(render);
-        init();
-    }
+    image.load(init);
 
 }
 
@@ -126,6 +129,27 @@ function initUniforms(){
     cameraLocation = gl.getUniformLocation(program, "uCamera");
     gl.uniform2f(cameraLocation, g.camera.x, g.camera.y);
 
+
+    var tileArray = [0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01];
+
+    tileLocation = gl.getUniformLocation(program, "uTile");
+    gl.uniform1fv(tileLocation, tileArray);
+
+// GLfloat v[10] = {...};
+// glUniform1fv(glGetUniformLocation(program, "v"), 10, v);
+
+    // lookup the sampler locations.
+    atlasTextureLocation = gl.getUniformLocation(program, "atlasTexture");
+    tileMapTextureLocation = gl.getUniformLocation(program, "tileMapTexture");
+    // set which texture units to render with.
+    gl.uniform1i(atlasTextureLocation, 0);  // texture unit 0
+    gl.uniform1i(tileMapTextureLocation, 1);  // texture unit 1
+  // Set each texture unit to use a particular texture.
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, atlasTexture);
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, tileMapTexture);
+
 }
 
 // ***********************************************************
@@ -201,6 +225,8 @@ function initBuffers(){
 
     }
 
+
+
     atlasTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, atlasTexture);
 
@@ -223,11 +249,55 @@ function initBuffers(){
     );
 
     gl.bindTexture(gl.TEXTURE_2D, atlasTexture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, atlas); // image into the texture.
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image.img['atlas'].file); // image into the texture.
 
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.enableVertexAttribArray(positionLocation);
     gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, atlasTexture);
+
+
+  // gl.activeTexture(gl.TEXTURE1);
+  // gl.bindTexture(gl.TEXTURE_2D, textures[1]);
+
+
+
+
+    tileMapTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, tileMapTexture);
+
+    // Set the parameters so we can render any size image.
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    // Create a buffer for the position of the rectangle corners.
+    tileMapBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tileMapBuffer);
+
+    setTile(1,1,tileMapArray);
+
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(tileMapArray),
+        gl.STATIC_DRAW
+    );
+
+    gl.bindTexture(gl.TEXTURE_2D, tileMapTexture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image.img['blank'].file); // image into the texture.
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, tileMapBuffer);
+    gl.enableVertexAttribArray(positionLocation);
+    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, tileMapTexture);
+
+
+
 
 }
 
@@ -253,8 +323,17 @@ function render() {
     // by running render loop multiple times, increasing until it dips below 17ms per frame
     for (var repeat=0; repeat<g.frame.repeat; repeat++){
 
+    gl.uniform1i(atlasTextureLocation, 0);  // texture unit 0
+    gl.uniform1i(tileMapTextureLocation, 1);  // texture unit 1
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, atlasTexture);
+        gl.activeTexture(gl.TEXTURE1);
+        gl.bindTexture(gl.TEXTURE_2D, tileMapTexture);
+
+
         gl.uniform2f(mouseLocation, g.mouse.x, g.mouse.y);
-        gl.uniform2f(cameraLocation, g.mouse.x, g.mouse.y);
+        //gl.uniform2f(cameraLocation, g.mouse.x, g.mouse.y);
 
 
         //gl.bindBuffer(gl.ARRAY_BUFFER, texture);
@@ -264,7 +343,8 @@ function render() {
         frameBuffer = gl.createFramebuffer();
         gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffer);
         //gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex, 0);
-        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, atlasTexture, 0);
+
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tileMapTexture, 0);
 
         gl.bindFramebuffer(gl.FRAMEBUFFER, null);
         gl.clearColor(1, 0, 1, 1); // red
